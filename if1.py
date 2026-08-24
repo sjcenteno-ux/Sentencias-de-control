460466cbdce9a2a0cec5cfa1d12a398a550fbd76
SantiagoRey <sjcenteno@uamv.edu.ni>
2026-08-24 09:38:45 -0600

    agregue el primer ejercicio

---
+++ if1.py
@@ -0,0 +1,7 @@
+#Leer la nota de un estudiante y decir si aprobo o su aprendizaje es inicial 
+grade = int(input("Ingrese la nota del estudiante:"))
+
+if grade >= 70:
+    print("Usted a aprobado")
+else :
+    print("Su aprendizaje es inicial")
